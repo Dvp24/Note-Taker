753b26237cd17d7d5042c0d50fda232f8e6e83b9
Dhanashree <dvphalak@gmail.com>
2018-11-01 00:15:52 -0400

    second

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,25 +1,25 @@
-// var router = require("express").Router();
-// var connection = require("../db/connection");
+var router = require("express").Router();
+var connection = require("../db/connection");
 
-// // GET route that finds all tables with isWaiting = false and sends back as JSON
-// // app.get("/api/home", function(req, res) {
+// GET route that finds all tables with isWaiting = false and sends back as JSON
+// app.get("/api/home", function(req, res) {
 
-// //   connection.query("SELECT * FROM tables WHERE isempty = false", (err, Data) => {
+//   connection.query("SELECT * FROM tables WHERE isempty = false", (err, Data) => {
 
-// //     if (err) {
-// //       console.log(err);
-// //       return res.status(500).end();
-// //     }
-// //     // send array of tables back
-// //     res.json(Data);
+//     if (err) {
+//       console.log(err);
+//       return res.status(500).end();
+//     }
+//     // send array of tables back
+//     res.json(Data);
 
-// //   });
+//   });
 
-// // });
-// router.post("/api/tables", function(req, res) {
+// });
+router.post("/api/home", function(req, res) {
 
-//   connection.query("SELECT * FROM tables WHERE isempty = false", function(err, Data){
+  connection.query("SELECT * FROM note WHERE isempty = false", function(err, Data){
     
-//   })
+  })
 
-// })
+})
--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,7 +1,7 @@
-// var router = require("express").Router();
-// var path = require("path");
+var router = require("express").Router();
+var path = require("path");
 
-// // how we set default page??
-// router.get("/home", function(req, res) {
-//   res.sendFile(path.join(__dirname, "../public/home.html"));
-// });
+// how we set default page??
+router.get("/home", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/home.html"));
+});
